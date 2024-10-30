f4d399269bf2dfb28bd10f96b43ada347c2bf509
tdog <161662619+tdogpog@users.noreply.github.com>
2024-10-29 21:24:11 -0500

    boilerplate for MainParent and router basics implemented in main

--- src/components/MainParent.jsx
+++ src/components/MainParent.jsx
@@ -1,8 +1,20 @@
 import React, { useEffect, useState } from "react";
 
+import Checkoutpage from "./childcomponents/Checkoutpage";
+import Homepage from "./childcomponents/Homepage";
+import ItemDescriptionpage from "./childcomponents/ItemDescriptionpage";
+import Shoppage from "./childcomponents/Shoppage";
 
-function MainParent(){
-    return()
+function MainParent() {
+  return (
+    <div className="pageContainer">
+      <div className="header"></div>
+      <Checkoutpage />
+      <Homepage />
+      <ItemDescriptionpage />
+      <Shoppage />
+    </div>
+  );
 }
 
-export default MainParent
+export default MainParent;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import MainParent from "./components/MainParent";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainParent />,
+    //nested routes
+    //that render within mainparent layout
+    //each child has path: url for route
+    //element: component for this route
+    //ex: /shop will render mainparent, within that, it
+    //will render shoppage component
+    children: [
+      { path: "/", element: <Homepage /> },
+      { path: "shop", element: <Shoppage /> },
+      //dynamic route for ids of any item
+      { path: "item/:id", element: <ItemDescriptionpage /> },
+      { path: "checkout", element: <Checkoutpage /> },
+    ],
+  },
+]);
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <MainParent />
+    <RouterProvider router={router} />
   </StrictMode>
 );
