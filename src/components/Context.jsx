19792db3c15004c3edc0f3a93cdbe85d70beba73
tdog <161662619+tdogpog@users.noreply.github.com>
2024-10-31 15:12:14 -0500

    Cart functionality updates

--- src/components/Context.jsx
+++ src/components/Context.jsx
@@ -10,19 +10,60 @@ export const CartContext = createContext();
 export const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
 
-  function addToCart(product) {
-    setCart();
-  }
+  const addToCart = (product) => {
+    //functional argument to a useState setter
+    //auto take in the useStates most recent state
+    setCart((prevCart) => {
+      const existingItem = prevCart.find((item) => item.id === product.id);
 
-  function removeFromCart(product){
+      if (existingItem) {
+        return prevCart.map((item) =>
+          item.id === product.id
+            ? //increment
+              { ...item, quantity: item.quantity + 1 }
+            : //else not match just return the item with no increment
+              item
+        );
+      } else {
+        //item not already in cart, spread cart and add to cart
+        return [...prevCart, { ...product, quantity: 1 }];
+      }
+    });
+  };
 
-  }
+  const decrementFromCart = (product) => {
+    setCart((prevCart) =>
+      prevCart
+        .map((item) =>
+          item.id === product.id
+            ? { ...item, quantity: item.quantity - 1 }
+            : item
+        )
+        .filter((item) => item.quantity > 0)
+    );
+  };
 
-  
+  const fullremoveFromCart = (product) => {
+    setCart((prevCart) => prevCart.filter((item) => item.id !== product.id));
+  };
+
+  const checkout = () => {};
+
+  const cartSize = () => {};
+
+  const cartPrice = () => {};
+
+  const contextValue = {
+    cart,
+    addToCart,
+    decrementFromCart,
+    fullremoveFromCart,
+    checkout,
+    cartSize,
+    cartPrice,
+  };
 
   return (
-    <CartContext.Provider value={}>
-      {children}
-    </CartContext.Provider>
+    <CartContext.Provider value={contextValue}>{children}</CartContext.Provider>
   );
 };
